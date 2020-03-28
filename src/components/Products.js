2aa1cd74e1f140c794257426b688ec16dbc3e68a
Ernestas Sukauskas <ernisx@gmail.com>
2020-03-28 18:30:13 +0200

    pagination

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
 import { Link }  from 'react-router-dom'
+import Pagination from './Pagination'
 
 class Products extends Component {
   constructor(props) {
     super(props) 
     this.state = {
-      products: []
+      products: [], //allCountries: []
+      currentProducts: [], //currentCountries
+      currentPage: null, 
+      totalPages: null
     }
   }
   
   componentDidMount() {
     this.getProducts()
+  }
+
+  onPageChanged = data => {
+    const { products } = this.state
+    const { currentPage, totalPages, pageLimit } = data
+
+    const offset = (currentPage - 1) * pageLimit
+    const currentProducts = products.slice(offset, offset + pageLimit)
+
+    this.setState({ currentPage, currentProducts, totalPages })
   }
 
   getProducts = _ => {
@@ -41,7 +55,11 @@ class Products extends Component {
 
   render() {
     //console.log(this.state);
-    const { products } = this.state;
+    const { products, currentProducts, currentPage, totalPages  } = this.state
+    const totalProducts = products.length
+    if (totalProducts === 0) return null
+    const headerClass = ['text-dark py-2 pr-4 m-0', currentPage ? 'border-gray border-right' : ''].join(' ').trim()
+
     let items = this.state.products
     if(this.state.filter) {
       items = items.filter((item) => {
@@ -52,6 +70,22 @@ class Products extends Component {
     return (
       <div>
         <div>
+          <div>
+            <h2 className={headerClass}>
+              <strong className="text-secondary">{totalProducts}</strong> Products 
+            </h2>
+            { currentPage && (
+              <span className="current-page d-inline-block h-100 pl-4 text-secondary">
+                Page <span className="font-weight-bold">{ currentPage }</span> / <span className="font-weight-bold">{ totalPages }</span>
+              </span>
+            ) }
+            </div>
+            <div className="d-flex flex-row py-4 align-items-center">
+              <Pagination totalRecords={totalProducts} pageLimit={9} pageNeighbours={1} onPageChanged={this.onPageChanged} />
+            </div>
+            { currentProducts.map(product => <p key={product.title}> {product.title} </p>) }
+          <div>
+        </div>
           <input type="text" onChange={this.filter.bind(this)} />
           {items.map((item, i) =>
           <Link to={`/products/${i}`}>
